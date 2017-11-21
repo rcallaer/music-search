260c3286cd041e882dd8babf240dcde79f50abbe
rallaer <ryandroidmail@gmail.com>
2017-11-21 05:42:05 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Profile from './Profile';
+import Gallery from './Gallery';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      query: '',
+      artist: null, //necessary for objects, I think
+      tracks: []
+    }
+  }
+
+  search() {
+    console.log("this.state", this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search';
+    // const FETCH_URL = BASE_URL + '?q=' + this.state.query
+    //                   + '&type=artist&limit=1';
+    //same as above with ES6 template strings
+    let FETCH_URL = `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
+    // console.log('FETCH_URL', FETCH_URL);
+    const ALBUM_URL = 'https://api.spotify.com/v1/artists';
+
+    //returns a promise
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0];
+      console.log('artist', artist);
+      this.setState({artist});
+
+      FETCH_URL = `${ALBUM_URL}/${artist.id}/top-tracks?country=US&`;
+      fetch(FETCH_URL, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => {
+        console.log('artist\'s top tracks:', json);
+        // const tracks = json.tracks;
+        const { tracks } = json; //ES6 shortcut same as above
+
+        this.setState({tracks});
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Music Master App</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search()
+                }
+              }}
+            />
+          <InputGroup.Addon onClick={() => this.search()}>
+            <Glyphicon glyph="search"></Glyphicon>
+          </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        {//hidden at first until artist isn't null
+          this.state.artist !== null
+          ?
+            <div>
+              <Profile
+                artist={this.state.artist}
+              />
+              <Gallery
+                tracks={this.state.tracks}
+              />
+            </div>
+          : <div></div>
+        }
+      </div>
+    )
+  }
+}
+
+export default App;
